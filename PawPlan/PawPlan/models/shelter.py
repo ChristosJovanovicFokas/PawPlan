8c2d6c37e4907d7cfa9f74ea4a7dac53a2f5ef19
cohand <131301401+cohand@users.noreply.github.com>
2024-03-07 15:55:00 -0500

    Create shelter.py

---
+++ PawPlan/PawPlan/models/shelter.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Shelter(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email_address = models.EmailField()
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
