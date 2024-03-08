95adb096b77eabf700a0c9b1c775ec38db6986b2
cohand <131301401+cohand@users.noreply.github.com>
2024-03-08 11:22:05 -0500

    Update shelter.py

--- PawPlan/PawPlan/models/shelter.py
+++ PawPlan/PawPlan/models/shelter.py
@@ -5,3 +5,4 @@ class Shelter(models.Model):
     phone_number = models.CharField(max_length=20)
     email_address = models.EmailField()
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    worker = models.ManytoManyField(worker)
