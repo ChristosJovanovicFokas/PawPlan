89d2e19c6deb808e2c794503259f4d15dc90717f
Ricky Krame <CorgiCluster@gmail.com>
2024-03-19 15:04:56 -0400

    Setup the html, view, and url. Able to query and display animal info.

--- pawplan/core/views.py
+++ pawplan/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Animal
 
 # Create your views here.
+
+def animal_list(request):
+    animal = Animal.objects.all()[:10]
+
+    return render(request, "animals.html", {
+        'animals' : animal
+    })
--- pawplan/pawplan/urls.py
+++ pawplan/pawplan/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('animals', views.animal_list, name='animal_list'),
 ]
