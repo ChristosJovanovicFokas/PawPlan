d57d230a739cf14e625ff8551e5d712067cd4c52
Stephen <piccol24@students.rowan.edu>
2024-03-20 17:41:29 -0400

    add basic task fetching for dashboard

--- pawplan/core/views.py
+++ pawplan/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Task, AnimalTask
+
 
 # Create your views here.
+def worker_dash(request):
+    tasks = Task.objects.all()
+    return render(
+        request,
+        "worker_dashboard.html",
+        {
+            "tasks": tasks,
+        },
+    )
