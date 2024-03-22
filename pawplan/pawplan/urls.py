aeac3ccb1c739ebc7c3fb309f8ebc85484247292
pruest77 <147074796+pruest77@users.noreply.github.com>
2024-03-22 11:23:11 -0400

    migrations

--- pawplan/pawplan/urls.py
+++ pawplan/pawplan/urls.py
@@ -24,16 +24,10 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name="home"),
     path("animals", views.animal_list, name="animal_list"),
     path("dashboard/", views.worker_dash, name="worker_dash"),
-<<<<<<< HEAD
-    path("about/", views.about_view, name="about"),
-]
-=======
     path("", views.home, name="home"),
     path('about/', views.about_view, name='about'),
     path('adopt/', views.adopt, name='adopt'),
     path('login/', views.login, name='login')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
->>>>>>> 02a38d4cf6bcc6ec72386a337a8e567805eab23d
