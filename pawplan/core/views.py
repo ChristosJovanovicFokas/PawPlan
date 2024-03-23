4a5060c7a33e988baa9ac54c1d8058c95fa6f543
Ricky Krame <CorgiCluster@gmail.com>
2024-03-23 12:49:58 -0400

    simple petlist setup.

--- pawplan/core/views.py
+++ pawplan/core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from .models import Animal
+from .models import Animal, Shelter
 
 # Create your views here.
 
 def animal_list(request):
     animal = Animal.objects.all()[:10]
 
+    sexOptions = list(Animal.objects.order_by().values_list('sex', flat=True).distinct())
+    colorOptions = list(Animal.objects.order_by().values_list('color', flat=True).distinct())
+    locationOptions = list(Shelter.objects.order_by().values_list('name', flat=True).distinct())
+
     return render(request, "animals.html", {
-        'animals' : animal
+        'animals' : animal,
+        'sexOptions' : sexOptions,
+        'colorOptions' : colorOptions,
+        'locationOptions' : locationOptions
     })
