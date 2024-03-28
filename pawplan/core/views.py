3ae21533ef561a7aafebb8a9bf42f6d5ad6af207
Stephen <piccol24@students.rowan.edu>
2024-03-28 14:45:53 -0400

    add filter and sort POST endpoints for dashboard

--- pawplan/core/views.py
+++ pawplan/core/views.py
@@ -12,6 +12,7 @@ def animal_list(request):
 
 def worker_dash(request):
     tasks = Task.objects.all()
+    animals = Animal.objects.all()
     return render(
         request,
         "worker_dashboard.html",
@@ -38,3 +39,42 @@ def about_view(request):
 def login(request):
 
     return render(request, "login.html", {})
+
+
+def filter_tasks(request):
+    """
+    A POST request to this view should contain a 'filter' parameter with a value of 'completed',
+    'incomplete', or 'all'. This view will return a list of tasks based on the filter value.
+    Additional filtering logic can be added as needed. See worker_dashboard.html for an idea of
+    how to structure the form that will send the POST request.
+    """
+    filter_value = request.POST.get("filter")
+    if filter_value == "completed":
+        tasks = Task.objects.filter(completion_datetime__isnull=False)
+    elif filter_value == "incomplete":
+        tasks = Task.objects.filter(completion_datetime__isnull=True)
+    else:
+        tasks = Task.objects.all()
+
+    # Assuming you have a 'tasks/task_list.html' template to render just the task list part
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+
+def sort_tasks(request):
+    """
+    A POST request to this view should contain a 'sort' parameter with a value of 'title',
+    'due_date', or 'creation_datetime'. This view will return a list of tasks sorted by the
+    specified field. Again, see worker_dashboard.html for an idea of how to structure the form
+    that will send the POST request.
+    """
+    sort_value = request.POST.get("sort")
+
+    # Define a default sort in case of unexpected sort values
+    sort_key = "title"
+    if sort_value in ["due_date", "creation_datetime"]:
+        sort_key = sort_value
+
+    tasks = Task.objects.all().order_by(sort_key)
+
+    # Assuming you have a 'tasks/task_list.html' template to render just the task list part
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
